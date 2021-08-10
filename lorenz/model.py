5e19b7c4428573fa71db1eb7802170851bb214ae
suranl <37652232+suranl@users.noreply.github.com>
2021-08-10 15:45:25 +0800

    Add files via upload

---
+++ lorenz/model.py
@@ -0,0 +1,173 @@
+from torch import nn
+import torch
+
+P=64
+def gaussian_init_(n_units, std=1):    
+    sampler = torch.distributions.Normal(torch.Tensor([0]), torch.Tensor([std/n_units]))
+    Omega = sampler.sample((n_units, n_units))[..., 0]  
+    return Omega
+
+
+class encoderNet(nn.Module):
+    def __init__(self, m, n, b, ALPHA = 1):
+        super(encoderNet, self).__init__()
+        self.N = m * n
+        self.tanh = nn.Tanh()
+
+        self.fc1 = nn.Linear(self.N, P*ALPHA)
+        self.fc2 = nn.Linear(P*ALPHA, P*ALPHA)
+        self.fc3 = nn.Linear(P*ALPHA, b)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0)          
+
+    def forward(self, x):
+        x = x.view(-1, 1, self.N)
+        x = self.tanh(self.fc1(x))
+        x = self.tanh(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
+
+
+class decoderNet(nn.Module):
+    def __init__(self, m, n, b, ALPHA = 1):
+        super(decoderNet, self).__init__()
+
+        self.m = m
+        self.n = n
+        self.b = b
+
+        self.tanh = nn.Tanh()
+
+        self.fc1 = nn.Linear(b, P*ALPHA)
+        self.fc2 = nn.Linear(P*ALPHA, P*ALPHA)
+        self.fc3 = nn.Linear(P*ALPHA, m*n)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0)          
+
+    def forward(self, x):
+        x = x.view(-1, 1, self.b)
+        x = self.tanh(self.fc1(x)) 
+        x = self.tanh(self.fc2(x))
+        x = self.fc3(x)
+        x = x.view(-1, 1, self.m, self.n)
+        return x
+
+
+
+class dynamicsC(nn.Module):
+    def __init__(self, b, init_scale):
+        super(dynamicsC, self).__init__()
+
+        self.dynamics = nn.Linear(b, b, bias=False)
+        self.fixed = nn.Linear(b, b-1, bias=False)
+        for p in self.parameters():
+            p.requires_grad=False
+        self.flexi = nn.Linear(b, 1, bias=False)
+
+        for j in range(0,b):
+            self.dynamics.weight.data[b-1][j]=self.flexi.weight.data[0][j]=0
+        self.dynamics.weight.data[b-1][0]=1
+
+        for i in range(0,b-1):
+            for j in range (0,b):
+                if i+1==j:
+                    self.dynamics.weight.data[i][j]=1
+                    self.fixed.weight.data[i][j]=1
+                else:
+                    self.dynamics.weight.data[i][j]=0
+                    self.fixed.weight.data[i][j]=0
+
+        print('----')
+        print(self.dynamics.weight)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        up = self.fixed(x)
+        down = self.flexi(x)
+        x = torch.cat((up,down),2)
+
+        self.dynamics.weight.data = torch.cat(( self.fixed.weight.data,self.flexi.weight.data),0)
+        #print("self.dynamics.weight.data=",self.dynamics.weight.data)
+        return x
+
+
+class dynamics_backD(nn.Module):
+    def __init__(self, b, omega):
+        super(dynamics_backD, self).__init__()
+        self.dynamics = nn.Linear(b, b, bias=False)
+        self.fixed = nn.Linear(b, b-1, bias=False)
+        for p in self.parameters():
+            p.requires_grad=False
+
+        self.flexi = nn.Linear(b, 1, bias=False)
+
+
+        for j in range(0,b-1):
+            self.dynamics.weight.data[0][j]=-omega.dynamics.weight.data[b-1][j+1]/omega.dynamics.weight.data[b-1][0]
+            self.flexi.weight.data[0][j]=self.dynamics.weight.data[0][j]
+        self.flexi.weight.data[0][b-1]=self.dynamics.weight.data[0][b-1]=1.0/omega.dynamics.weight.data[b-1][0]
+
+        for i in range(1,b):
+            for j in range (0,b):
+                if i-1==j:
+                    self.dynamics.weight.data[i][j]=1
+                    self.fixed.weight.data[i-1][j]=1
+                else:
+                    self.dynamics.weight.data[i][j]=0
+                    self.fixed.weight.data[i-1][j]=0
+
+        #print(self.dynamics.weight)
+        #print(self.flexi.weight)
+
+
+    def forward(self, x):
+        up = self.flexi(x)
+        down = self.fixed(x)
+        x = torch.cat((up,down),2)
+        self.dynamics.weight.data = torch.cat(( self.flexi.weight.data,self.fixed.weight.data),0)
+        return x
+
+
+class embed_koopmanAE(nn.Module):
+    def __init__(self, m, n, b, steps, steps_back, alpha = 1, init_scale=1):
+        super(embed_koopmanAE, self).__init__()
+        self.steps = steps
+        self.steps_back = steps_back
+        
+        self.encoder = encoderNet(m, n, b, ALPHA = alpha)
+        self.dynamics = dynamicsC(b, init_scale)
+        self.backdynamics = dynamics_backD(b, self.dynamics)
+        self.decoder = decoderNet(m, n, b, ALPHA = alpha)
+
+
+    def forward(self, x, mode='forward'):
+        out = []
+        out_back = []
+        z = self.encoder(x.contiguous())
+        q = z.contiguous()
+
+        
+        if mode == 'forward':
+            for _ in range(self.steps):
+                q = self.dynamics(q)
+                out.append(self.decoder(q))
+
+            out.append(self.decoder(z.contiguous())) 
+            return out, out_back    
+
+        if mode == 'backward':
+            for _ in range(self.steps_back):
+                q = self.backdynamics(q)
+                out_back.append(self.decoder(q))
+                
+            out_back.append(self.decoder(z.contiguous()))
+            return out, out_back
---
+++ lorenz/read_dataset.py
@@ -0,0 +1,84 @@
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
+from matplotlib import pylab as plt
+from scipy.special import ellipj, ellipk
+
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib as mpl
+
+import torch
+import pandas as pd
+
+#******************************************************************************
+# Read in data
+#******************************************************************************
+def data_from_name(name, noise = 0.0, theta=2.4):   
+    if name == 'lorenz':
+        return lorenz()    
+    else:
+        raise ValueError('dataset {} not recognized'.format(name))
+
+
+def rescale(Xsmall, Xsmall_test):
+    #******************************************************************************
+    # Rescale data
+    #******************************************************************************
+    Xmin = Xsmall.min()
+    Xmax = Xsmall.max()
+    
+    Xsmall = ((Xsmall - Xmin) / (Xmax - Xmin)) 
+    Xsmall_test = ((Xsmall_test - Xmin) / (Xmax - Xmin)) 
+
+    return Xsmall, Xsmall_test
+
+
+def lorenz():
+    xs, ys, zs = [], [], []
+
+    a, b, c = 10.0, 28.0, 8.0 / 3.0         
+    h = 0.003   #h=0.006                          
+    x0, y0, z0 = 0.1, 0, 0
+    for i in range(5000):
+        x1 = x0 + h * a * (y0 - x0)
+        y1 = y0 + h * (x0 * (b - z0) - y0)
+        z1 = z0 + h * (x0 * y0 - c * z0)
+        x0, y0, z0 = x1, y1, z1
+        xs.append(x0)
+        ys.append(y0)
+        zs.append(z0)
+    X=[]
+    X.append(xs)
+    X.append(ys)
+    X.append(zs)
+    X=np.array(X).T
+    Xclean=X.copy()
+
+    noise=0.00  #noise=0.01
+    X[:,0] += np.random.standard_normal(X[:,0].shape) * noise
+    X[:,1] += np.random.standard_normal(X[:,1].shape) * noise
+    X[:,2] += np.random.standard_normal(X[:,2].shape) * noise
+
+    # Rotate to high-dimensional space
+    Q = np.random.standard_normal((96,3))
+    Q,_ = np.linalg.qr(Q)
+    
+    X = X.dot(Q.T) # rotate
+    Xclean = Xclean.dot(Q.T)
+    
+
+    X = 2 * (X - np.min(X)) / np.ptp(X) - 1
+    Xclean = 2 * (Xclean - np.min(Xclean)) / np.ptp(Xclean) - 1
+    train_num = 1000
+
+    X_train = X[:train_num]   
+    X_test = X[train_num:1050]
+    X_train_clean = Xclean[:train_num]   
+    X_test_clean = Xclean[train_num:1050]
+
+
+    return X_train, X_test, X_train_clean, X_test_clean, 96, 1
+
